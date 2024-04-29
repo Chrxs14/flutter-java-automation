cf2ec4dfb0633d76fae5f8ed5f8450552da0de6f
chrxs14 <trabajo.ochoachristopher@gmail.com>
2024-04-29 14:39:18 -0500

    Created: view for flutter app automation

---
+++ src/main/java/com/flutterautomation/views/CounterAppView.java
@@ -0,0 +1,9 @@
+package com.flutterautomation.views;
+
+public class CounterAppView {
+    public static final String APP_TITLE = "counter_app_title";
+    public static final String COUNTER_TEXT = "counter_value";
+    public static final String RESET_BUTTON = "reset_button";
+    public static final String INCREMENT_BUTTON = "increment_button";
+    public static final String DECREMEMT_BUTTON = "decrement_button";
+}
